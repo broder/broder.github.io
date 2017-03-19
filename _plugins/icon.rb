7cc8671e4dd6d89931532c029b7de4c24e523782
David Broder-Rodgers <broder93@gmail.com>
2017-03-19 13:35:13 +0000

    Add icon tag

---
+++ _plugins/icon.rb
@@ -0,0 +1,23 @@
+module Jekyll
+  module Tags
+    class Icon < Liquid::Tag
+      class << self
+        def tag_name
+          self.name.split("::").last.downcase
+        end
+      end
+
+      def initialize(tag_name, icon, tokens)
+        super
+
+        @icon = icon.strip
+      end
+
+      def render(context)
+        "<i class=\"fa fa-fw fa-lg #{@icon}\"></i>"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag(Jekyll::Tags::Icon.tag_name, Jekyll::Tags::Icon)
